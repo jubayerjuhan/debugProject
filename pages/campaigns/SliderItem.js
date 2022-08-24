1d04df4e343afe8e48d7488533ee7e59ea1e1b8d
Jubayer Juhan <davidjuhan23@gmail.com>
2022-08-24 23:08:49 +0600

    first commit

---
+++ pages/campaigns/SliderItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SliderItem = ({ isGlobal }) => {
+  return (
+    <div
+      className={`${
+        campaign.isGoal ? "h-[300px] w-[450px]" : "h-[400px] w-[650px]"
+      }   bg-red  flex items-center justify-center panel`}
+    >
+      <p className="text-white"> items-center 1</p>
+    </div>
+  );
+};
+
+export default SliderItem;
